2f42f81d80953cdeb9dd6c7a6a5fdf4d15a9de0c
Андрей Киреев <air_kireev@bk.ru>
2021-10-03 16:52:30 +0700

    Recursion DONE

--- Recursion/Main.cpp
+++ Recursion/Main.cpp
@@ -1,8 +1,31 @@
 //Recursion, Factorial, Power
 #include<iostream>
 using namespace std;
+
+//#define ELEVATOR
 //#define FACTORIAL_RECURSION
-#define POWER_RECURSION
+//#define POWER_RECURSION
+
+
+void elevator(int level)
+{
+	if (level == 0)return;
+		cout << level << endl;
+	elevator(level - 1);//рекурсивный вызов
+}
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	//cout << "Hello World";
+	//main();//рекурсивный вызов функции main
+
+#ifdef ELEVATOR
+	int n;
+	cout << "¬ведите номер этажа: ";
+	cin >> n;
+	elevator(n);
+#endif // ELEVATOR
+}
 
 #ifdef POWER_RECURSION
 double power = 1;
